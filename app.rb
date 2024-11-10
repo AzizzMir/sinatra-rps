e4cf3e60f01b41fd98bdb4f158d069462773a8d5
Handamier Mieraizizi <ezizmir9@gmail.com>
2024-11-10 20:02:00 +0000

    made RPS game website

--- app.rb
+++ app.rb
@@ -1,9 +1,54 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
+end
+
+#rock route
+get("/rock") do
+  options = ["rock", "paper", "scissors"]
+  @option = options.sample
+    if @option == "paper"
+      @result = "lost"
+    elsif @option == "scissors"
+      @result = "won"
+    else
+      @result = "tied"
+    end
+  erb(:rock)
+end
+
+#paper route
+get("/paper") do
+  options = ["rock", "paper", "scissors"]
+  @option = options.sample
+    if @option == "scissors"
+      @result = "lost"
+    elsif @option == "rock"
+      @result = "won"
+    else
+      @result = "tied"
+    end
+  erb(:paper)
+end
+
+#scissors route
+get("/scissors") do
+  options = ["rock", "paper", "scissors"]
+  @option = options.sample
+    if @option == "rock"
+      @result = "lost"
+    elsif @option == "paper"
+      @result = "won"
+    else
+      @result = "tied"
+    end
+  erb(:scissors)
 end
